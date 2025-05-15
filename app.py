3ae53a0afbe8fceed656ad2c96278f8c332a4838
clamar92 <cla.mar92@gmail.com>
2025-05-15 10:04:19 +0200

    First commit

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask_restx import Api
+import os
 
 from extensions import db
 from resources.thoughts import api as thoughts_ns
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("La variabile DATABASE_URL non e' stata trovata!)")
 
 def create_app(config_overrides=None):
 
     app = Flask(__name__)
 
     #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///thoughts.db"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:adminpass@db/thoughtsdb"
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     if config_overrides:
